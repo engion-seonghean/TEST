562f95064a7a3fe23354239817249116e7c7b9df
engion-seonghean <88641289+engion-seonghean@users.noreply.github.com>
2022-10-25 19:48:06 +0900

    Update Program.cs
    Add Web_[main]modify1947

--- TESTGIT/TESTGIT/Program.cs
+++ TESTGIT/TESTGIT/Program.cs
@@ -8,14 +8,12 @@ namespace TESTGIT
 {
     static class Program
     {
-        //Add VS_[main]init1222
-        //Add Web_[main]init1241
-        //Add Web_[main]modify1252
-        //Add VS_[main]Modify1253
-        //Add VS_[main]Modify1459
-        //Add Web_[main]modify1458
-
         //Add VS_[Branch6]Modify1903
+         //Add Web_[main]modify1947
+
+        
+        
+        
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
