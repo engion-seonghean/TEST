b4ee0335097282fb79cb0e6852f62117c12ee039
성환 문 <seonghwan.mun@engionsystem.co.kr>
2022-10-25 12:53:47 +0900

    //Add VS_[main]Modify1253

--- TESTGIT/TESTGIT/Program.cs
+++ TESTGIT/TESTGIT/Program.cs
@@ -10,6 +10,7 @@ namespace TESTGIT
     {
         //Add VS_[main]init1222
         //Add Web_[main]init1241
+        //Add VS_[main]Modify1253
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
