3c9bde4055c607741d09b2a11e04775cdcf995fd
성환 문 <seonghwan.mun@engionsystem.co.kr>
2022-10-25 12:32:12 +0900

    //Add VS_init1222

--- TESTGIT/TESTGIT/Program.cs
+++ TESTGIT/TESTGIT/Program.cs
@@ -35,7 +35,7 @@ namespace TESTGIT
         #endregion
 
         //Add VS_init1207
-
+        //Add VS_[main]init1222
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
