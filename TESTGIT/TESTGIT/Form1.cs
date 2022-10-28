6a99def997616ec88d6f8236cd0b2aa18a5ef77d
성환 문 <seonghwan.mun@engionsystem.co.kr>
2022-10-28 12:31:56 +0900

    //Add VS_[Main]modify1231

--- TESTGIT/TESTGIT/Form1.cs
+++ TESTGIT/TESTGIT/Form1.cs
@@ -19,20 +19,7 @@ namespace TESTGIT
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Add VS_[main]modify2344
-            //Add VS_[Branch]modify2346
-            //Add Web_[main]modify2353
-            //Add VS_[Branch]modify2354
-
-            //Add Web_[main]modify0001
-            //Add VS_[Branch]modify0003
-
-            //Add VS_[Branch]modify0011
-            //Add Web_[main]modify0010
-            
-             //Add Web_[main]modify0017
-
-            //Add VS_[Branch]modify0020
+            //Add VS_[Main]modify1231
         }
     }
 }
