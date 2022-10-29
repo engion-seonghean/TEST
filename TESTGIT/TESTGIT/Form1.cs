b6791139cbf8bbe6f018f8538e4c774662eae884
성환 문 <seonghwan.mun@engionsystem.co.kr>
2022-10-29 17:04:36 +0900

    m`init

--- TESTGIT/TESTGIT/Form1.cs
+++ TESTGIT/TESTGIT/Form1.cs
@@ -12,10 +12,11 @@ namespace TESTGIT
 {
     public partial class Form1 : Form
     {
-        //check
-        //check1
-        //check2
-        //main3
+        //pull -원격 저장소의 내용을 가져와 자동으로 병합 작업을 실행
+        //fetch 단순히 원격 저장소의 내용을 확인만 하고 로컬 데이터와 병합은 하고 싶지 않은 경우
+        //push, 로컬 저장소의 데이터를 원격 저장소로 밀어넣기
+
+
         public Form1()
         {
             InitializeComponent();
