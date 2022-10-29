90203500bf0da0b8f0a6972ae5d7767c8fd37ae8
성환 문 <seonghwan.mun@engionsystem.co.kr>
2022-10-29 21:11:21 +0900

    b`merge//b`mod

--- TESTGIT/TESTGIT/Form1.cs
+++ TESTGIT/TESTGIT/Form1.cs
@@ -15,9 +15,14 @@ namespace TESTGIT
         //pull -원격 저장소의 내용을 가져와 자동으로 병합 작업을 실행
         //fetch 단순히 원격 저장소의 내용을 확인만 하고 로컬 데이터와 병합은 하고 싶지 않은 경우
         //push, 로컬 저장소의 데이터를 원격 저장소로 밀어넣기
+        //스태시 -변경사항을 커밋을 하지 않고, 킵해두기
+        //체리픽- 커밋 하나만 지금 브랜치에 붙이기
+        //리셋 -  HEAD: 이후 커밋이 없어짐
+        //리셋 - MIXED: 이후 커밋이 수정됨 상태로 유지
 
         //make branch
         //m`mod
+        //b`mod
         public Form1()
         {
             InitializeComponent();
