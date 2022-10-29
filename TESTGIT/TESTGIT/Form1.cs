d4b2c70254536a418d96c8bc234d9d522b0b0e97
성환 문 <seonghwan.mun@engionsystem.co.kr>
2022-10-29 17:58:20 +0900

    //m`mod

--- TESTGIT/TESTGIT/Form1.cs
+++ TESTGIT/TESTGIT/Form1.cs
@@ -16,7 +16,7 @@ namespace TESTGIT
         //fetch 단순히 원격 저장소의 내용을 확인만 하고 로컬 데이터와 병합은 하고 싶지 않은 경우
         //push, 로컬 저장소의 데이터를 원격 저장소로 밀어넣기
 
-
+        //m`mod
         public Form1()
         {
             InitializeComponent();
